71c02e42df6bcb2f91f7822201424f20eb413d42
Willthompson99 <will.j.thompson@outlook.com>
2025-07-03 18:18:29 -0500

    commit

--- models/staging/stg_tax_credits.sql
+++ models/staging/stg_tax_credits.sql
@@ -5,6 +5,10 @@ SELECT
   TAX_YEAR,
   RECIPIENT_NAME,
   CREDIT_TYPE,
-  AMOUNT,
+  -- Clean the AMOUNT field by removing commas and converting to numeric
+  CASE 
+    WHEN AMOUNT IS NULL OR AMOUNT = '' THEN 0
+    ELSE TRY_CAST(REPLACE(AMOUNT, ',', '') AS DECIMAL(15,2))
+  END AS amount,
   CREDIT_DESCRIPTION
 FROM {{ source('willthompson', 'tax_credits_2023') }}
